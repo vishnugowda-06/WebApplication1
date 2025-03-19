6d9c3014e9b82cc6c2d373bfc6e393a0a269efab
vishnu.pg <vishnu.pg@TLAPT27407>
2025-03-19 14:48:18 +0530

    Add project files.

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+      public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApplication2/DATA/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.DATA
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
